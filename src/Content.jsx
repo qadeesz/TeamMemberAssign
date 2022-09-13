6e0eea4e6f80a8ed6e9bf9894862142d86a5d2c1
Qadees Javed <qadeesjaved9098@gmail.com>
2022-09-13 18:22:42 +0000

    Initial commit

---
+++ src/Content.jsx
@@ -0,0 +1,12 @@
+import Employees from './Employees';
+
+
+const Content = () => {
+  return (
+    <main>
+      <Employees />
+    </main>
+  )
+}
+
+export default Content
---
+++ src/Footer.jsx
@@ -0,0 +1,9 @@
+const Footer = () => {
+  return (
+    <footer>
+      <h1>FOoter</h1>
+    </footer>
+  )
+}
+
+export default Footer
